4545f72ffcec1f0b0694575ea668ff930ce58422
riazkhan186 <riazuddinbappy52@gmail.com>
2023-05-30 16:04:02 +0600

    responsive menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,4 +10,15 @@
   $("#popupSearchButton").on("click", function () {
     $(".popupSearchBar").toggle();
   });
+
+
+  $(document).on('click', function(event) {
+    var $div = $('#popupSearchButton');
+    var $target = $(event.target);
+    if (!$div.is($target) && !$div.has($target).length && !$target.is('input')) {
+      $('.popupSearchBar').hide();
+    }
+  });
+  
+
 })(jQuery);
