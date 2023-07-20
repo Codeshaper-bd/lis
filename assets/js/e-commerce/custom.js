470bef1ce12c2ac1501d50d51772b7c8bcd37eef
riazkhan186 <riazuddinbappy52@gmail.com>
2023-07-20 17:26:50 +0600

    fix:product preview slider

--- assets/js/e-commerce/custom.js
+++ assets/js/e-commerce/custom.js
@@ -109,32 +109,28 @@
   };
 
   //file-upload
-  const dropArea = document.querySelector(".drop_box"),
-    button = dropArea.querySelector("button"),
-    dragText = dropArea.querySelector("header"),
-    input = dropArea.querySelector("input");
-  let file;
-  var filename;
+  // const dropArea = document.querySelector(".drop_box"),
+  //   button = dropArea.querySelector("button"),
+  //   dragText = dropArea.querySelector("header"),
+  //   input = dropArea.querySelector("input");
+  // let file;
+  // var filename;
 
-  button.onclick = () => {
-    input.click();
-  };
+  // button.onclick = () => {
+  //   input.click();
+  // };
 
-  input.addEventListener("change", function (e) {
-    var fileName = e.target.files[0].name;
-    let filedata = `
-    <form action="" method="post">
-    <div class="form">
-    <h4>${fileName}</h4>
-    </div>
-    </form>`;
-    dropArea.innerHTML = filedata;
-  });
+  // input.addEventListener("change", function (e) {
+  //   var fileName = e.target.files[0].name;
+  //   let filedata = `
+  //   <form action="" method="post">
+  //   <div class="form">
+  //   <h4>${fileName}</h4>
+  //   </div>
+  //   </form>`;
+  //   dropArea.innerHTML = filedata;
+  // });
 
   // <input type="email" placeholder="Enter email upload file">
   //   <button class="btn">Upload</button>
-
-  
-
-
 })(jQuery);
